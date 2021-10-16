89fd1bc3efbaa4179590e7517828a6c9bb4f71ae
David Avendaño <d.avendano.c@hotmail.com>
2021-10-16 15:33:25 -0500

    Initial setup

--- NotesApp WatchKit Extension/ContentView.swift
+++ NotesApp WatchKit Extension/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
             .padding()
+            .foregroundColor(Color.black)
+            .background(Color.white)
     }
 }
 
