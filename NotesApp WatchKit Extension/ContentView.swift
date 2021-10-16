3d8833efe654ef4165ac08d4ce61352ccef9efde
David Avendaño <d.avendano.c@hotmail.com>
2021-10-16 13:39:07 -0500

    Initial Commit

---
+++ NotesApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by David Avendaño on 16/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesApp WatchKit Extension/NotesAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesAppApp.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by David Avendaño on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
