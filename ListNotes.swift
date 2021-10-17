2ea9cacbd42baf89365fd764adf487533425ebbb
David Avendaño <d.avendano.c@hotmail.com>
2021-10-17 00:25:51 -0500

    Listing notes in a view

---
+++ ListNotes.swift
@@ -0,0 +1,30 @@
+//
+//  ListNotes.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by David Avendaño on 17/10/21.
+//
+
+import SwiftUI
+
+struct ListNotes: View {
+    
+    var notes: [Note] = [Note(title: "1"),
+                          Note(title: "2"),
+                          Note(title: "3")]
+    
+    var body: some View {
+        List{
+            ForEach(0..<notes.count, id: \.self ){ i in
+                Text("\(notes[i].title) - \(notes[i].creationDate)")
+                    .lineLimit(1)
+            }
+        }
+    }
+}
+
+struct ListNotes_Previews: PreviewProvider {
+    static var previews: some View {
+        ListNotes()
+    }
+}
